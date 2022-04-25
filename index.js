e8299ec2dd9087b12d6faa3669a4ba60e1a702e3
jaycobobob <jacobtheedwards@gmail.com>
2022-04-24 23:32:59 -0700

    Fixed issue with dynamically listening to port

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ for (let handler of Object.values(handlers)) {
     app.use(handler);
 }
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Listening to port ${PORT}`);
 });
