a68f382b91e3657ea86827c362050f2a34963b63
jaycobobob <jacobtheedwards@gmail.com>
2022-04-24 23:40:35 -0700

    Fixed issue with connecting to database

--- database/db.js
+++ database/db.js
@@ -2,8 +2,9 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const connectionString = `postgress://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_DATABASEPORT}/${process.env.DB_DATABASE}`;
+console.log(connectionString);
 const connection = {
-    connectionString: process.env.DATABASE_URL ? process.env.DATABASEURL : connectionString,
+    connectionString: process.env.DATABASE_URL ? process.env.DATABASE_URL : connectionString,
     ssl: { rejectUnauthorized: false },
 };
 
