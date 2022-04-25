06101f5888aaaf300ec6e87c06f54ea2ccf64d5a
jaycobobob <jacobtheedwards@gmail.com>
2022-04-24 23:18:59 -0700

    Added Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: npodeindex.js
--- handlers/get.js
+++ handlers/get.js
@@ -3,6 +3,7 @@ const db = require("../database/db");
 
 const router = express();
 
+// Not sure how to implement this. Need to get more info on user_location, best-matched sorting
 router.get("/search", (req, res) => {
     res.json({ done: true, result: "This method is not yet implemented." });
 });
